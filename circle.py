2ff034e70f3089d4087ff74ac82d7745b44a0282
Saul Rodriguez <saulargz35@gmail.com>
2021-11-28 17:18:16 -0800

    Created circle class and implemented game mechanics

---
+++ circle.py
@@ -0,0 +1,54 @@
+import pygame
+import random
+
+
+class Circle:
+    def __init__(self, screen, color, radius):
+        self.screen = screen
+        self.color = color
+        self.radius = radius
+        self.circle_x = random.randint(50, 450)
+        self.circle_y = random.randint(50, 450)
+
+    def draw_circle(self):
+        """
+        Draws circle object
+        :return: None
+        """
+        pygame.draw.circle(self.screen, self.color, (self.circle_x, self.circle_y), self.radius, 0)
+
+    def mouse_collide(self, mx, my):
+        """
+        Checks if mouse collides with circle
+        :param mx: int
+        :param my: int
+        :return: bool
+        """
+        if Circle.distance(self.circle_x, mx, self.circle_y, my) <= 50:
+            return True
+        return False
+
+    def delete(self):
+        rect = pygame.Rect(self.circle_x - 50, self.circle_y - 50, 100, 100)
+        pygame.draw.rect(self.screen, (0, 0, 0), rect)
+
+    def set_color(self, color):
+        """
+        Sets color of circle
+        :param color: tuple[int, int, int]
+        :return: None
+        """
+        self.color = color
+
+    @staticmethod
+    def distance(x1, x2, y1, y2):
+        """
+        Calculates distance
+        :param x1: int
+        :param x2: int
+        :param y1: int
+        :param y2: int
+        :return: int
+        """
+        return (((x2 - x1)**2) + ((y2 - y1)**2))**(1/2)
+
--- main.py
+++ main.py
@@ -1,33 +1,57 @@
-import pygame, sys
+import pygame
+import sys
+from circle import Circle
 
 clock = pygame.time.Clock()
 
 from pygame.locals import *
 
-pygame.init()
-
-pygame.display.set_caption("Circle Clicker")
-
 WINDOW_SIZE = (700, 500)
 
+pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+pygame.display.set_caption("Circle Clicker")
+
+display = pygame.Surface((700, 500))
+
+circle = Circle(screen, (255, 0, 0), 50)
 
 
-def run_game_events():
+def run_game_events(mx, my):
+    global circle
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == MOUSEBUTTONDOWN and circle.mouse_collide(mx, my):
+            circle.delete()
+            circle = Circle(screen, (255, 0, 0), 50)
+            circle.draw_circle()
 
 
 def main():
+    global circle
     while True:
         screen.fill((0, 0, 0))
 
-        run_game_events()
-        pygame.display.update()
-        clock.tick(60)
+        mx, my = pygame.mouse.get_pos()
 
+        circle.draw_circle()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == MOUSEBUTTONDOWN and circle.mouse_collide(mx, my):
+                circle.delete()
+                circle = Circle(screen, (255, 0, 0), 50)
+                circle.draw_circle()
+
+        # surf = pygame.transform.scale(display, WINDOW_SIZE)
+        # screen.blit(surf, (0, 0))
+        pygame.display.update()
+
+        clock.tick(60)
 
 if "__main__" == __name__:
     main()

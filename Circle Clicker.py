8a442528ccbd22e8eb909f229c9a02cc6aa82563
Saul Rodriguez <saulargz35@gmail.com>
2021-12-19 14:39:28 -0800

    Added gui for settings

--- Circle Clicker.py
+++ Circle Clicker.py
@@ -6,21 +6,29 @@ from scripts.game_timer import Timer
 from scripts.window import Window
 from scripts.text import Text
 from scripts.button import Button
+from scripts.text_box import TextBox
 
 clock = pygame.time.Clock()
 timer = Timer(clock)
 
 from pygame.locals import *
 
+
+def center(window: pygame.Surface, surface: pygame.Surface):
+    return window.get_width() // 2 - surface.get_width() // 2
+
+
 WINDOW_SIZE = (700, 500)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
-
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (143, 143, 143)
 pygame.init()
 
 screen = Window(WINDOW_SIZE, "Circle Clicker").get_screen()
 
-title_text = Text("Circle Clicker", "assets/arcade_font.TTF", 32, (100, 20), BLUE)
+title_text = Text(100, 20, "Circle Clicker", "assets/arcade_font.TTF", 32, BLUE)
 
 font = pygame.font.Font("assets/arcade_font.TTF", 32)
 
@@ -28,9 +36,21 @@ circle = Circle(screen, (255, 0, 0), 50)
 
 start_img = pygame.image.load("assets/start.png")
 exit_img = pygame.image.load("assets/exit.png")
+settings_img = pygame.image.load("assets/settings.png")
+apply_img = pygame.image.load("assets/apply.png")
+cancel_img = pygame.image.load("assets/cancel.png")
 
-start_btn = Button(WINDOW_SIZE[0] / 2 - 65, WINDOW_SIZE[1] / 2 - 70, start_img, 1)
-exit_btn = Button(WINDOW_SIZE[0] / 2 - 50, WINDOW_SIZE[1] / 2 - 20, exit_img, 1)
+start_btn = Button(center(screen, start_img), 200, start_img, 1)
+exit_btn = Button(center(screen, exit_img), 350, exit_img, 1)
+settings_btn = Button(center(screen, settings_img), 275, settings_img, 1)
+apply_btn = Button(center(screen, apply_img), 220, apply_img, 1)
+cancel_btn = Button(center(screen, cancel_img), 320, cancel_img, 1)
+
+minute_text_box = TextBox(WINDOW_SIZE[0]//2 - 130, 150, 130, 40, GREY, WHITE)
+seconds_text_box = TextBox(WINDOW_SIZE[0]//2 + 10, 150, 130, 40, GREY, WHITE)
+
+minutes_text = Text(WINDOW_SIZE[0]//2 - 113, 100, "Minutes", "assets/arcade_font.TTF", 32, BLUE)
+seconds_text = Text(WINDOW_SIZE[0]//2 + 17, 100, "Seconds", "assets/arcade_font.TTF", 32, BLUE)
 
 score = 0
 
@@ -56,35 +76,86 @@ def run_game_events(mx, my):
             circle.draw_circle()
 
 
+def init_settings_menu():
+    running = True
+    while running:
+        mx, my = pygame.mouse.get_pos()
+
+        screen.fill(BLACK)
+
+        minute_text_box.draw_box(screen, mx, my, GREY)
+        seconds_text_box.draw_box(screen, mx, my, GREY)
+        screen.blit(minutes_text.get_text(), minutes_text.get_text_rect())
+        screen.blit(seconds_text.get_text(), seconds_text.get_text_rect())
+
+        if minute_text_box.isSelected():
+            minute_text_box.draw_box(screen, mx, my, WHITE)
+        else:
+            minute_text_box.draw_box(screen, mx, my, GREY)
+
+        if seconds_text_box.isSelected():
+            seconds_text_box.draw_box(screen, mx, my, WHITE)
+        else:
+            seconds_text_box.draw_box(screen, mx, my, GREY)
+
+        if apply_btn.draw(screen):
+            running = False
+
+        if cancel_btn.draw(screen):
+            running = False
+
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONDOWN and minute_text_box.get_rect().collidepoint(mx, my):
+                minute_text_box.selected = True
+                seconds_text_box.selected = False
+
+            if event.type == MOUSEBUTTONDOWN and seconds_text_box.get_rect().collidepoint(mx, my):
+                minute_text_box.selected = False
+                seconds_text_box.selected = True
+
+            if event.type == QUIT:
+                pygame.quit()
+
+        print(minute_text_box.isSelected())
+
+        pygame.display.update()
+
+        clock.tick(60)
+
+
 start_game = False
 
 
 def main():
     global circle, score, start_game
-    run = True
-    while run:
+    running = True
+    while running:
+
+        screen.fill(BLACK)
 
         mx, my = pygame.mouse.get_pos()
 
         if not start_game:
 
             title_text.set_font_size("Circle Clicker", 64)
-            title_text.set_center_cords((WINDOW_SIZE[0]/2, 50))
+            title_text.set_cords(center(screen, title_text.get_text()), 50)
             screen.blit(title_text.get_text(), title_text.get_text_rect())
 
             if start_btn.draw(screen):
                 start_game = True
+            if settings_btn.draw(screen):
+                init_settings_menu()
             if exit_btn.draw(screen):
-                run = False
+                running = False
 
         else:
 
             timer.start()
-            timer_text = Text(timer.get_time(), "assets/arcade_font.TTF", 32, (30, 20), RED, None)
+            timer_text = Text(30, 20, timer.get_time(), "assets/arcade_font.TTF", 32, RED)
 
-            score_text = Text(f"Score: {score}", "assets/arcade_font.TTF", 32, (600, 20), (0, 0, 255))
+            score_text = Text(565, 20, f"Score: {score}", "assets/arcade_font.TTF", 32, BLUE)
 
-            screen.fill((0, 0, 0))
+            screen.fill(BLACK)
             screen.blit(score_text.get_text(), score_text.get_text_rect())
             screen.blit(timer_text.get_text(), timer_text.get_text_rect())
 
@@ -92,8 +163,8 @@ def main():
 
             if timer.is_finished():
                 circle.delete()
-                screen.fill((0, 0, 0))
-                score_text.get_text_rect().center = ((WINDOW_SIZE[0] / 2), (WINDOW_SIZE[1] / 2))
+                screen.fill(BLACK)
+                score_text.get_text_rect().center = ((WINDOW_SIZE[0] // 2), (WINDOW_SIZE[1] // 2))
                 screen.blit(score_text.get_text(), score_text.get_text_rect())
 
         run_game_events(mx, my)
--- scripts/text.py
+++ scripts/text.py
@@ -2,24 +2,24 @@ import pygame
 
 
 class Text:
-    def __init__(self, text, font_file: str, font_size: int, center_cords, text_color: tuple, background_text_color=None):
+    def __init__(self, x, y, text, font_file: str, font_size: int, text_color: tuple, background_text_color=None):
         self.font_file = font_file
         self.font_size = font_size
         self.text_color = text_color
         self.background_text_color = background_text_color
-        self.center_cords = center_cords
         self.font = pygame.font.Font(font_file, font_size)
         self.text = self.font.render(text, True, self.text_color)
         self.text_rect = self.text.get_rect()
-        self.text_rect.center = center_cords
+        self.text_rect.x = x
+        self.text_rect.y = y
 
     def delete(self, screen, screen_size):
         rect = pygame.Rect(self.text_rect.x, self.text_rect.y,  self.text_rect.width, self.text_rect.height )
         pygame.draw.rect(screen, (255, 0, 0), rect)
 
-    def set_center_cords(self, center_cords):
-        self.center_cords = center_cords
-        self.text_rect.center = center_cords
+    def set_cords(self, x, y):
+        self.text_rect.x = x
+        self.text_rect.y = y
 
     def set_font_size(self, text, font_size):
         self.font_size = font_size
@@ -32,3 +32,6 @@ class Text:
 
     def get_text(self):
         return self.text
+
+    def get_width(self):
+        return self.text.get_width()
